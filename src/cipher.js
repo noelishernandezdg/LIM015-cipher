0a1e9f4c76c3517c403fa515234b9a866176e131
noelishernandezdg <noelishernandezdagraca@gmail.com>
2021-06-10 15:12:43 -0500

    Primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,22 @@
 const cipher = {
-  // ...
+  encode: function (offSet, textOne) {
+    console.log(offSet + textOne)
+//Primero hay que recorer el string (bucle for)
+    for (let i = 0; i < textOne.length; i++) {
+      console.log(textOne[i])
+      let textAscii = textOne.charCodeAt(i)
+      if (textAscii >= 65 && text <= 90) {
+        let newText = ((text - 65 + offSet) % 26) + 65;
+        console.log(newText);
+        
+        // let uni = text.String.fromCharCode(i)
+      
+
+
+      }
+    }
+//obteber CharCodeAt por cada elemento del string
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
+//buscando el boton de decifrar y poner un escuhador de click y 
+document.getElementById("buttonCipher").addEventListener("click", prueba)
+
+function prueba() {
+    let offSet = document.getElementById("offSet").value;
+    let string = document.getElementById("textOne").value.toUpperCase();
+    let resultado = document.getElementById("textTwo");
+    resultado.value = string; 
+
+    cipher.encode(offSet,string)
+}
+
+
+
+
+
 
 console.log(cipher);
